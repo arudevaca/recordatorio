080731db1a0e3032f9b6c8b91bd142ed97aea6be
Aru Devaca <aruzetav@gmail.com>
2025-02-25 18:16:32 -0300

    "Cambio en comentarios"

--- recordatorio.py
+++ recordatorio.py
@@ -1,20 +1,20 @@
-import time # time tiene funciones para manejar el tiempo en Python.
-import schedule # Permite programar tareas automáticas.
-from plyer import notification # Importa la función notification de la librería plyer. Plyer es una librería en Python que permite enviar notificaciones en Windows, macOS y Linux.
+import time # importamos la libreria time, que tiene funciones para manejar el tiempo en Python.
+import schedule # importamos la libreria schedule, que permite programar tareas automáticas.
+from plyer import notification # importamos la función notification de la librería plyer. Plyer es una librería en Python que permite enviar notificaciones en Windows, macOS y Linux.
 
-def enviar_notificacion():
-    notification.notify( # Llama a la función notify() que crea una notificación emergente en el sistema operativo.
-        title="Recordatorio", # Define el título de la notificación 
-        message="¡No olvides hacer una pausa y estirarte!", # Define el mensaje de la notificación
-        timeout= 8  # La notificación se muestra durante 8 segundos
+def enviar_notificacion(): # definimos una función de enviar notificación
+    notification.notify( # llamamos a la función notify() que crea una notificación emergente
+        title="Recordatorio", # definimos el título de la notificación 
+        message="¡No olvides hacer una pausa y estirarte!", # definimos el mensaje de la notificación
+        timeout= 8  # definimos el tiempo que se muestra la notificación a 8 segundos
     )
 
-schedule.every(1).hours.do(enviar_notificacion) # Programa la función enviar_notificacion() para que se ejecute cada 1 hora.
+schedule.every(1).minute.do(enviar_notificacion) # programamos la función enviar_notificacion() para que se ejecute cada 1 minuto.
 
-try:
-    while True:
-        schedule.run_pending()
-        time.sleep(60)  # Esperar 60 segundos antes de revisar nuevamente, pausa la ejecución del programa por un tiempo determinado.
-except KeyboardInterrupt:
-    print("\nPrograma detenido por el usuario.")
+try: # iniciamos un bucle infinito para ejecutar las tareas programadas de forma continua
+    while True: 
+        schedule.run_pending() # ejecuta las tareas programadas que estén pendientes  
+        time.sleep(60)  # espera 60 segundos antes de revisar nuevamente, pausa la ejecución del programa por un tiempo determinado.
+except KeyboardInterrupt: # captura la interrupción del usuario (Ctrl + C)  
+    print("\nPrograma detenido por el usuario.") # mensaje de salida cuando el usuario detiene el programa 
 
